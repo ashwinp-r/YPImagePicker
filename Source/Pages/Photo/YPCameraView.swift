337e4204b3e359cb215fb86de5551ead8fdb75d1
Ashwin R <ashwinr14@gmail.com>
2019-02-17 21:59:31 -0800

    Modified YPCameraView to allow for full screen photo/video only when YPSquare picker is set to false (default is true).

--- Source/Pages/Photo/YPCameraView.swift
+++ Source/Pages/Photo/YPCameraView.swift
@@ -53,16 +53,39 @@ class YPCameraView: UIView, UIGestureRecognizerDelegate {
         // Layout
         let isIphone4 = UIScreen.main.bounds.height == 480
         let sideMargin: CGFloat = isIphone4 ? 20 : 0
-        layout(
-            0,
-            |-sideMargin-previewViewContainer-sideMargin-|,
-            -2,
-            |progressBar|,
-            0,
-            |buttonsContainer|,
-            0
-        )
-        previewViewContainer.heightEqualsWidth()
+
+		// Ashwin_Patch_Begin
+		// Modified to allow for full screen video/photo
+        if YPConfig.onlySquareImagesFromCamera
+        {
+            layout(
+                0,
+                |-sideMargin-previewViewContainer-sideMargin-|,
+                -2,
+                |progressBar|,
+                0,
+                |buttonsContainer|,
+                0
+            )
+            previewViewContainer.heightEqualsWidth()
+
+            
+        }else
+        {
+            layout(
+                0,
+                |-sideMargin-previewViewContainer-sideMargin-|,
+                -2,
+                |progressBar|,
+                0
+            )
+            previewViewContainer.fillContainer()
+            
+            buttonsContainer.fillHorizontally()
+            buttonsContainer.height(100)
+            buttonsContainer.Bottom == previewViewContainer.Bottom - 15
+        }
+		// Ashwin_Patch_End
 
         overlayView?.followEdges(previewViewContainer)
 
