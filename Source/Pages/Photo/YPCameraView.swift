95de923f98c862e7b52d253e2d85b639f8ba3748
Ashwin R <ashwinr14@gmail.com>
2019-02-19 02:14:10 -0800

    IT WORKS!

--- Source/Pages/Photo/YPCameraView.swift
+++ Source/Pages/Photo/YPCameraView.swift
@@ -1,4 +1,4 @@
-//
+///
 //  YPCameraView.swift
 //  YPImgePicker
 //
@@ -19,7 +19,7 @@ class YPCameraView: UIView, UIGestureRecognizerDelegate {
     let flashButton = UIButton()
     let timeElapsedLabel = UILabel()
     let progressBar = UIProgressView()
-
+    
     convenience init(overlayView: UIView? = nil) {
         self.init(frame: .zero)
         
@@ -53,9 +53,9 @@ class YPCameraView: UIView, UIGestureRecognizerDelegate {
         // Layout
         let isIphone4 = UIScreen.main.bounds.height == 480
         let sideMargin: CGFloat = isIphone4 ? 20 : 0
-
-		// Ashwin_Patch_Begin
-		// Modified to allow for full screen video/photo
+        
+        // Ashwin_Patch_Begin
+        // Modified to allow for full screen video/photo
         if YPConfig.onlySquareImagesFromCamera
         {
             layout(
@@ -68,7 +68,7 @@ class YPCameraView: UIView, UIGestureRecognizerDelegate {
                 0
             )
             previewViewContainer.heightEqualsWidth()
-
+            
             
         }else
         {
@@ -79,28 +79,27 @@ class YPCameraView: UIView, UIGestureRecognizerDelegate {
                 |progressBar|,
                 0
             )
-//            previewViewContainer.fillContainer(200)
-            
-//            buttonsContainer.fillHorizontally()
+            previewViewContainer.fillContainer()
+            buttonsContainer.fillHorizontally()
             buttonsContainer.height(100)
-//            buttonsContainer.Bottom == previewViewContainer.Bottom - 15
+            buttonsContainer.Bottom == previewViewContainer.Bottom - 15
         }
-		// Ashwin_Patch_End
-
+        // Ashwin_Patch_End
+        
         overlayView?.followEdges(previewViewContainer)
-
+        
         |-(15+sideMargin)-flashButton.size(42)
-//        flashButton.Bottom == previewViewContainer.Bottom - 15
-
+        flashButton.Bottom == previewViewContainer.Bottom - 15
+        
         flipButton.size(42)-(15+sideMargin)-|
-//        flipButton.Bottom == previewViewContainer.Bottom - 15
+        flipButton.Bottom == previewViewContainer.Bottom - 15
         
         timeElapsedLabel-(15+sideMargin)-|
         timeElapsedLabel.Top == previewViewContainer.Top + 15
         
         shotButton.centerVertically()
         shotButton.size(84).centerHorizontally()
-
+        
         // Style
         backgroundColor = YPConfig.colors.photoVideoScreenBackground
         previewViewContainer.backgroundColor = .black
