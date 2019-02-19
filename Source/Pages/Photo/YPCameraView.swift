7c5a9266676f31b7ec243b21452f28f5fb725be0
Ashwin R <ashwinr14@gmail.com>
2019-02-18 20:52:20 -0800

    Attempt 1

--- Source/Pages/Photo/YPCameraView.swift
+++ Source/Pages/Photo/YPCameraView.swift
@@ -77,13 +77,15 @@ class YPCameraView: UIView, UIGestureRecognizerDelegate {
                 |-sideMargin-previewViewContainer-sideMargin-|,
                 -2,
                 |progressBar|,
+                0,
+                |buttonsContainer|,
                 0
             )
             previewViewContainer.fillContainer()
             
-            buttonsContainer.fillHorizontally()
-            buttonsContainer.height(100)
-            buttonsContainer.Bottom == previewViewContainer.Bottom - 15
+//            buttonsContainer.fillHorizontally()
+//            buttonsContainer.height(100)
+//            buttonsContainer.Bottom == previewViewContainer.Bottom - 15
         }
 		// Ashwin_Patch_End
 
