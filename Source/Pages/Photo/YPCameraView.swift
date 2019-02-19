5f8c966f7921c9bba5972cb6897cf4eb3ab71dcf
Ashwin R <ashwinr14@gmail.com>
2019-02-19 01:25:25 -0800

    8

--- Source/Pages/Photo/YPCameraView.swift
+++ Source/Pages/Photo/YPCameraView.swift
@@ -90,9 +90,11 @@ class YPCameraView: UIView, UIGestureRecognizerDelegate {
         overlayView?.followEdges(previewViewContainer)
 
         |-(15+sideMargin)-flashButton.size(42)
+        flashButton.followEdges(previewViewContainer)
 //        flashButton.Bottom == previewViewContainer.Bottom - 15
 
         flipButton.size(42)-(15+sideMargin)-|
+        flipButton.followEdges(previewViewContainer)
 //        flipButton.Bottom == previewViewContainer.Bottom - 15
         
         timeElapsedLabel-(15+sideMargin)-|
