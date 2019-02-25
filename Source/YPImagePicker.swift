f3568806a99c85623961346288b2b6eab3c900c9
Ashwin R <ashwinr14@gmail.com>
2019-02-24 23:39:47 -0800

    Allow for altering of tint/navigation item colors.

--- Source/YPImagePicker.swift
+++ Source/YPImagePicker.swift
@@ -143,9 +143,9 @@ public class YPImagePicker: UINavigationController {
         }
         
         // If user has not customized the Nav Bar tintColor, then use black.
-        if UINavigationBar.appearance().tintColor == nil {
-            UINavigationBar.appearance().tintColor  = .black
-        }
+//        if UINavigationBar.appearance().tintColor == nil {
+//            UINavigationBar.appearance().tintColor  = .black
+//        }
     }
     
     deinit {
